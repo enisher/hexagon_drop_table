30a77d93f8d90af8a6603c2b63c5f203b0731a7f
pesua <pesua.mail@gmail.com>
2017-09-30 13:33:13 +0300

    Simple smoke tests

--- src/test/java/com/anahoret/anadea_bot/AnadeaBotJavaApplicationTests.java
+++ src/test/java/com/anahoret/anadea_bot/AnadeaBotJavaApplicationTests.java
@@ -1,16 +1,74 @@
 package com.anahoret.anadea_bot;
 
+import com.anahoret.anadea_bot.controller.api.GameController;
+import com.anahoret.anadea_bot.dto.Board;
+import com.anahoret.anadea_bot.dto.Game;
+import com.anahoret.anadea_bot.dto.ServerMove;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class AnadeaBotJavaApplicationTests {
 
+	@Autowired
+	GameController gameController;
+
 	@Test
-	public void contextLoads() {
+	public void smoke() {
+		Game game = new Game();
+		game.setId("1");
+		game.setFirst_turn(true);
+		game.setTraining(false);
+		game.setJumps(new HashMap<String, Integer>());
+
+		Board board = new Board();
+		board.setCells(new int[][]{
+				new int[]{-1, 1, 0, 2, -1},
+      			new int[]{0, -1, 0, 0, -1},
+				new int[]{2, 0, 0, 0, 1},
+				new int[]{0, 0, 0, -1, -1},
+				new int[]{-1, 1, 0, 2, -1}
+		});
+		board.setSize(3);
+		game.setBoard(board);
+
+		gameController.startGame(game);
+		gameController.makeMove("1", 1);
+		ServerMove serverMove = new ServerMove();
+		serverMove.setJumps(new HashMap<String, Integer>(){{
+			put("1", 2);
+			put("2", 3);
+		}});
+		serverMove.setChanges(new int[][]{
+				new int[]{0,5,2,0},
+				new int[]{0,3,0,2},
+		});
+		serverMove.setChanges(new int[1][5]);
+		gameController.handleMove("1", serverMove);
+
+		gameController.finishGame("1");
 	}
 
+	@Test
+	public void testEmptyState() throws Exception {
+		gameController.makeMove("1", 1);
+
+		ServerMove serverMove = new ServerMove();
+		serverMove.setJumps(new HashMap<String, Integer>(){{
+			put("1", 2);
+			put("2", 3);
+		}});
+		serverMove.setChanges(new int[][]{
+				new int[]{0,5,2,0},
+				new int[]{0,3,0,2},
+		});
+		serverMove.setChanges(new int[1][5]);
+		gameController.handleMove("1", serverMove);
+	}
 }
